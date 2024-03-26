3668c6816c007a3540c99f66f86d34ef7856b48b
Brajesh Singh <brajesh_singh@persistent.com>
2024-03-26 20:51:13 +0530

    login controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,43 +1,70 @@
-const asyncHandler = require('async-handler-express')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const User = require('../models/user')
 
-// const signUp = (req, res, next) => {
-//     const { name, email, password } = req.body;
 
-//     bcrypt.hash(password, 12)
-//     .then(hashedPassword => {
-//         const user = new User({
-//             name,
-//             email,
-//             hashedPassword
-//         })
+const signUp = (req, res, next) => {
+    const { name, email, password, role } = req.body;
 
-//         return user.save()
-//     })
-//     .then((result) => {
-//         res.status(201).json({message: "User successfuly created", data: result })
-//     }).catch((err) => {
-//         throw new Error(err)
-//     })
-// }
+    User.findOne({ email })
+    .then((checkUser) => {
+        if (checkUser) {
+            res.status(200).json({message: "Email already in use!"})
+        } else {
+            bcrypt.hash(password, 12)
+            .then(hashedPassword => {
+                const user = new User({
+                    name,
+                    email,
+                    hashedPassword,
+                    role
+                })
 
-const signUp = asyncHandler(async (req, res) => {
-    const {name, email, password, role} = req.body;
-    const hashedPassword = await bcrypt.hash(password, 12);
-    res.json({ hashedPassword });
-    // console.log(hashedPassword);
-    // exit;
-});
+                return user.save()
+            }).then((result) => {
+                res.status(201).json({message: "User successfuly created", data: result })
+            }).catch((err) => {
+                throw new Error(err)
+            })
+        }
+    })
+    .catch((err) => {
+        throw new Error(err)
+    })
+}
 
-// asyncHandler(async (req, res) => {
-//     // Asynchronous operation
-//     const result = await someAsyncOperation();
-//     res.json({ result });
-//   })
 
 const login = (req, res, next) => {
+    const { email, password } = req.body
+    let loadedUser;
+    
+    User.findOne({ email })
+    .then((userCheck) => {
+        if (!userCheck) {
+            throw new Error("User not exist, Please create account or contact to admin!")
+        } else if (userCheck.status === 1) {
+            throw new Error("User account is not active, Please contact to admin!")
+        }
+        loadedUser = userCheck;
+        return bcrypt.compare(password, loadedUser.password)
+    }).then((isEqual) => {
+        if (!isEqual) {
+            throw new Error("Invalid credential. Please check your email or password!")
+        }
 
+        const token = jwt.sign(
+            {
+                email: loadedUser.email,
+                userId: loadedUser._id.toString()
+            },
+            'somesupersecretsecret',
+            { expiresIn: '1h' }
+        );
+
+        res.status(200).json({ token: token, userId: loadedUser._id.toString() })
+    }).catch((err) => {
+        throw new Error("Error", err)
+    })
 }
 
 module.exports = { signUp, login }
