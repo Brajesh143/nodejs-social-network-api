9c83e7be5b5405be5954ef7da55fad51db620438
Brajesh143 <brajesh.singhania143@gmail.com>
2024-03-27 01:44:43 +0530

    added asyncHandler

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
-const asyncHandler = require('async-handler-express')
+const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const User = require('../models/user')
 
 // const signUp = (req, res, next) => {
@@ -22,22 +23,60 @@ const User = require('../models/user')
 //     })
 // }
 
-const signUp = asyncHandler(async (req, res) => {
-    const {name, email, password, role} = req.body;
-    const hashedPassword = await bcrypt.hash(password, 12);
-    res.json({ hashedPassword });
-    // console.log(hashedPassword);
-    // exit;
+const signUp = asyncHandler(async (req, res, next) => {
+    const {name, email, password, role, status} = req.body;
+    const hashedPassword = await bcrypt.hash(password, 12)
+
+    try {
+        const checkUser = await User.findOne({ email })
+        if (checkUser) {
+            throw new Error("Email already in use. Please use another email")
+        }
+
+        const newUser = await User.create({
+            name, email, hashedPassword, role, status
+        });
+
+        if (newUser) {
+            res.status(201).json({ message: "User created successfuly", data: newUser })
+        }
+    } catch (err) {
+        throw new Error(err)
+    }
 });
 
-// asyncHandler(async (req, res) => {
-//     // Asynchronous operation
-//     const result = await someAsyncOperation();
-//     res.json({ result });
-//   })
+const login = asyncHandler(async (req, res, next) => {
+    const { email,  password} = req.body;
+    
+    // try {
+        let userData;
+        const checkEmail = await User.findOne({ "email": email });
+        userData = checkEmail;
+        console.log(userData, userData.password)
+        if (!checkEmail) {
+            throw new Error("Account not exist. Please create new account!")
+        } else if (checkEmail.status === 'Inactive') {
+            throw new Error("Your account is inactive. Please contact to admin!")
+        }
 
-const login = (req, res, next) => {
+        // const hashedPassword = await bcrypt.hash(password, 12);
+        const comparePassword = await bcrypt.compare(password, checkEmail.password)
+        console.log("compahgh", comparePassword)
+        // if (comparePassword) {
+        //     const token = jwt.sign(
+        //         {
+        //             email: checkEmail.email,
+        //             userId: checkEmail._id.toString()
+        //         },
+        //         'somesupersecretsecret',
+        //         {expiresIn: '1h' }
+        //     )
 
-}
+        //     res.status(200).json({message: "User login successfuly!", token: token})
+        // }
+    // } catch (err) {
+    //     throw new Error(err)
+    // }
+})
 
 module.exports = { signUp, login }
